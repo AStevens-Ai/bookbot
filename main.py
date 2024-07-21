572e782790c30b5b1937bacebf0321ea4ff3af12
astevens-ai <astevens81602@gmail.com>
2024-07-21 12:05:37 -0400

    added function to get character count of text argument.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main() :
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     num_of_words = get_word_count(text)
-    print(f"{num_of_words} words found in the document")
+    count_of_char = get_char_count(text)
+    print(f"{count_of_char}")
        
 
 def get_word_count(text):
@@ -14,6 +15,18 @@ def get_word_count(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_char_count(text):
+    count_dict = {}
+    my_string = text
+    low_case_string = my_string.lower()
+    character = list(low_case_string)
+    for char in character:
+        if char not in count_dict:
+            count_dict[char] = 1
+        elif char in count_dict:
+            count_dict[char] += 1
+    return count_dict
 
 
 main()
